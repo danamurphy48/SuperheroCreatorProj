5f9e3ee83654c0a5e5d349f4e14972f56458f8e8
danamurphy48 <danamurphy48@gmail.com>
2020-05-18 13:45:34 -0500

    Added properties to Superhero

--- SuperheroCreator/Models/Superhero.cs
+++ SuperheroCreator/Models/Superhero.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,12 @@ namespace SuperheroCreator.Models
 {
     public class Superhero
     {
+        [Key]
+        public string Name;
+        public string AlterEgo;
+        public string PrimaryAbility;
+        public string SecondaryAbility;
+        public string Catchphrase;
+
     }
 }
